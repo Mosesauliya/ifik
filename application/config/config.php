0b3201b7032e8bab209dfc7619e2e9f86a60dec0
unknown <alifmuzakki1110@gmail.com>
2026-09-18 09:37:00 +0700

    feat(config): add auto-detect multi-environment for NAS domain and database credentials

--- application/config/config.php
+++ application/config/config.php
@@ -23,10 +23,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$root = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://")
-        . ($_SERVER['HTTP_HOST'] ?? 'localhost')
-        . str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
-$config['base_url'] = rtrim($root, '/') . '/';
+if (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], 'ifik.forzasoftwarehouse.com') !== false) {
+    $config['base_url'] = 'https://ifik.forzasoftwarehouse.com/';
+} else {
+    $root = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://")
+            . ($_SERVER['HTTP_HOST'] ?? 'localhost')
+            . str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+    $config['base_url'] = rtrim($root, '/') . '/';
+}
 
 /*
 |--------------------------------------------------------------------------
