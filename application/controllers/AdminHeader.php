b1667f41bb49fe8e1937d78ffe00f3c3b54ad253
lilromawaffle-droid <lilromawaffle@gmail.com>
2026-09-21 05:18:43 +0700

    a

--- application/controllers/AdminHeader.php
+++ application/controllers/AdminHeader.php
@@ -9,12 +9,12 @@ class AdminHeader extends CI_Controller {
         $this->load->model('Header_model');
         $this->load->helper(array('form', 'url'));
 
-        // // Pastikan hanya admin (1), kaur (2), atau laboran (21) yang bisa akses
-        // $role_id = (int)$this->session->userdata('role_id');
-        // if (!$this->session->userdata('logged_in') || ($role_id !== 1 && $role_id !== 2 && $role_id !== 21)) {
-        //     $this->session->set_flashdata('error', 'Akses ditolak. Anda tidak memiliki izin untuk halaman ini.');
-        //     redirect('login');
-        // }
+         // Pastikan hanya admin (1), kaur (2), atau laboran (21) yang bisa akses
+         $role_id = (int)$this->session->userdata('role_id');
+         if (!$this->session->userdata('logged_in') || ($role_id !== 1 && $role_id !== 2 && $role_id !== 21)) {
+             $this->session->set_flashdata('error', 'Akses ditolak. Anda tidak memiliki izin untuk halaman ini.');
+             redirect('login');
+         }
     }
 
     public function index()
