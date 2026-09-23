4ae25811a7ae5131a90e56650e9f00f0b85abb1b
Mosesauliya <mosesauliya@gmail.com>
2026-09-23 21:08:21 +0700

    revert: Restore .htaccess and database.php to upstream state

--- application/config/database.php
+++ application/config/database.php
@@ -73,47 +73,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$is_local_win = (
-	(isset($_SERVER['HTTP_HOST']) && (
-		strpos($_SERVER['HTTP_HOST'], 'localhost') !== false ||
-		strpos($_SERVER['HTTP_HOST'], '127.0.0.1') !== false
-	)) ||
-	PHP_OS_FAMILY === 'Windows'
-);
-
 $is_nas = (
 	(isset($_SERVER['HTTP_HOST']) && (
 		strpos($_SERVER['HTTP_HOST'], 'ifik.forzasoftwarehouse.com') !== false ||
 		strpos($_SERVER['HTTP_HOST'], 'nas') !== false ||
 		strpos($_SERVER['HTTP_HOST'], '192.168.') !== false
 	)) ||
-	(PHP_OS_FAMILY === 'Linux' && !isset($_SERVER['HTTP_HOST']))
+	PHP_OS_FAMILY === 'Linux'
 );
 
-if ($is_local_win) {
-	// === DATABASE LOCAL XAMPP / WINDOWS ===
-	$db['default'] = array(
-		'dsn'	=> '',
-		'hostname' => 'localhost',
-		'username' => 'root',
-		'password' => '',
-		'database' => 'db_ifik_baru',
-		'dbdriver' => 'mysqli',
-		'dbprefix' => '',
-		'pconnect' => FALSE,
-		'db_debug' => (ENVIRONMENT !== 'production'),
-		'cache_on' => FALSE,
-		'cachedir' => '',
-		'char_set' => 'utf8',
-		'dbcollat' => 'utf8_general_ci',
-		'swap_pre' => '',
-		'encrypt' => FALSE,
-		'compress' => FALSE,
-		'stricton' => FALSE,
-		'failover' => array(),
-		'save_queries' => TRUE
-	);
-} elseif ($is_nas) {
+if ($is_nas) {
 	// === DATABASE NAS (PRODUCTION / SERVER) ===
 	$db['default'] = array(
 		'dsn'	=> '',
@@ -137,7 +106,7 @@ if ($is_local_win) {
 		'save_queries' => TRUE
 	);
 } else {
-	// === DATABASE TAILSCALE / REMOTE ===
+	// === DATABASE LOCAL / LARAGON / TAILSCALE ===
 	$db['default'] = array(
 		'dsn'	=> '',
 		'hostname' => '100.83.19.18',
